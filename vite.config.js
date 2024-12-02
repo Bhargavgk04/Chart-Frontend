c19baa70f15bae0ca6f9fec93d0ece1ae935b209
Bhargav Govind Katkam <bhargavkatkam0@gmail.com>
2024-12-02 14:49:15 +0530

    routes

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,9 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    server: {
+        host: '0.0.0.0',
+        port: process.env.PORT || 3000, // Use the PORT environment variable or default to 3000
     },
-  },
-})
+});
+
